9777f1f84639afbf54ebb7cc010f78a4644d6c55
lwj <214245088@qq.com>
2021-12-03 10:02:15 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 
 let isProd = process.env.NODE_ENV === 'production';
+let publicPath = '/vue3-basic';
 let configObj = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath
 };
 module.exports = configObj;
